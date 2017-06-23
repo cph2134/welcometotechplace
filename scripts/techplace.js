c5a55301a462f77d7ee4285a9f744332b420ca74
China Hoffman <china.hoffman@gmail.com>
2017-06-22 21:04:33 -0400

    update

--- scripts/techplace.js
+++ scripts/techplace.js
@@ -1,4 +1,33 @@
 //Javascript
+
+//function to reveal techEtiquette101 Video and take the quiz button//
 document.getElementById("videoBtn").addEventListener("click", function(){
     document.getElementById("techEtiquette101").style.display = "block"
   });
+
+//Array of test questions
+
+var quiz = [
+  {
+  "Question": "Is Michael Scott the world\'s best boss?",
+  "A": "Yes"
+  "B": "Who is Michael Scott?"
+  "C": "Depends"
+  "D": "No"
+  },
+  {
+    "Question": "",
+    "A": ""
+    "B": ""
+    "C":
+    "D":
+  },
+  {
+    "Question": "Did Michael Scott learn anything from sensitivity training?",
+    "A":
+    "B":
+    "C":
+    "D":
+  }
+
+];
