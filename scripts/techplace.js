c7aa95f2f9d46c53cf515207106b77edb3a4fcac
China Hoffman <china.hoffman@gmail.com>
2017-06-24 13:32:38 -0400

    update
    submit totals and returns alerts

--- scripts/techplace.js
+++ scripts/techplace.js
@@ -29,7 +29,7 @@ var quiz = [
       "Office etiquette isn't funny",
       "All the good ones are proprietary"
     ],
-    answer: "All the good ones are propriatary"
+    answer: "All the good ones are proprietary"
   },
   {
     id: 3,
@@ -43,7 +43,8 @@ var quiz = [
     answer: "Clearly, yes"
   }
 ];
-/* right now this code works by printing out q1 and the answers for q1, then over
+/* Function to generate HTML for quiz questions by looping through array.
+right now this code works by printing out q1 and the answers for q1, then over
 writing it with q1 + q1 answers + q2 + q2answers, etc. That is inefficient and not
 really what I want to happen. If I have time, I want to see if I can fix it.*/
 
@@ -66,6 +67,23 @@ quizBtnEl.addEventListener("click", function(){
   }
 });
 
+/*function to reveal submit button when "take the quiz" button is clicked.*/
 document.querySelector("#officeQuiz").addEventListener("click", function(){
     document.querySelector("#checkQuiz").style.display = "block"
-  });
+});
+
+/* function to check quiz answers and return messages.*/
+document.querySelector("#checkQuiz").addEventListener("click", function(){
+  var total = 0;
+  for (var i = 0; i < quiz.length; i++) {
+    var answer1 = document.querySelector(`input[name=q-${i}]:checked`).value;
+      if (answer1 === quiz[i].answer) {
+        total++;
+      }
+  }
+  if (total == 3) {
+    alert("Perfect score! Great job.");
+  }
+  else {alert(`You got ${total} out of 3. Maybe you should watch the video again.`);
+}
+});
