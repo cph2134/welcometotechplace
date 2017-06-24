e9c0d1d8e1aefc23b68bc93479f8283dcc5b2479
China Hoffman <china.hoffman@gmail.com>
2017-06-23 20:51:24 -0400

    quiz questions version 1
    JS works to loop through array and create questions and create answers, but JS is creating each answer separately rather than through a second loop.

--- scripts/techplace.js
+++ scripts/techplace.js
@@ -1,32 +1,80 @@
 //Javascript
 
 //function to reveal techEtiquette101 Video and take the quiz button//
-document.getElementById("videoBtn").addEventListener("click", function(){
-    document.getElementById("techEtiquette101").style.display = "block"
+document.querySelector("#videoBtn").addEventListener("click", function(){
+    document.querySelector("#techEtiquette101").style.display = "block"
   });
+  // Important Elements
+  var quizBtnEl = document.querySelector('#officeQuiz');
+  var quizEl = document.querySelector('#quizQuestions');
 
 //Array of test questions
-
-/*  {
-  "Question": "Is Michael Scott the world\'s best boss?",
-  "A": "Yes"
-  "B": "Who is Michael Scott?"
-  "C": "Depends"
-  "D": "No"
+var quiz = [
+  {
+    id: 1,
+    question: "Is Michael Scott the world\'s best boss?",
+    options: [
+      "Yes",
+      "Who is Michael Scott?",
+      "No",
+      "I don't know"
+    ],
+    answer: "No"
   },
   {
-    "Question": "A second question",
-    "A": ""
-    "B":
-    "C":
-    "D":
+    id: 2,
+    question: "Why aren\'t there more funny office etiquette videos on YouTube?",
+    options: [
+      "No idea",
+      "Office etiquette isn't funny",
+      "All the good ones are proprietary"
+    ],
+    answer: "All the good ones are propriatary"
   },
   {
-    "Question": "Did Michael Scott learn anything from sensitivity training?",
-    "A":
-    "B":
-    "C":
-    "D":
+    id: 3,
+    question: "Did Michael Scott learn anything from sensitivity training?",
+    options: [
+      "Clearly, yes",
+      "Clearly, no",
+      "I didn't watch the video",
+      "No idea"
+    ],
+    answer: "Clearly, yes"
   }
+];
 
-];*/
+
+quizBtnEl.addEventListener("click", function(){
+  var quizHTML = "";
+  for (var i = 0; i < quiz.length; i++) {
+    quizHTML += `<li id="">`;
+    quizHTML += quiz[i].question;
+    quizHTML += `</li>`;
+    quizHTML += `<div class="radio">`;
+    quizHTML += `<label>`;
+    quizHTML += `<input type="radio" name="q-${i}" value="${quiz[i].options[0]}">`;
+    quizHTML += `${quiz[i].options[0]}`;
+    quizHTML += `</label>`;
+    quizHTML += `</div>`;
+    quizHTML += `<div class="radio">`;
+    quizHTML += `<label>`;
+    quizHTML += `<input type="radio" name="q-${i}" value="${quiz[i].options[1]}">`;
+    quizHTML += `${quiz[i].options[1]}`;
+    quizHTML += `</label>`;
+    quizHTML += `</div>`;
+    quizHTML += `<div class="radio">`;
+    quizHTML += `<label>`;
+    quizHTML += `<input type="radio" name="q-${i}" value="${quiz[i].options[2]}">`;
+    quizHTML += `${quiz[i].options[2]}`;
+    quizHTML += `</label>`;
+    quizHTML += `</div>`;
+    quizHTML += `<div class="radio">`;
+    quizHTML += `<label>`;
+    quizHTML += `<input type="radio" name="q-${i}" value="${quiz[i].options[3]}">`;
+    quizHTML += `${quiz[i].options[3]}`;
+    quizHTML += `</label>`;
+    quizHTML += `</div>`;
+      quizEl.innerHTML = quizHTML;
+  }
+});
