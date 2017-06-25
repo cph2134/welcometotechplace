aa9528db4a81592a64c85d5197c28896b16fe0ab
China Hoffman <china.hoffman@gmail.com>
2017-06-25 15:00:49 -0400

    cleaning up

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # welcometotechplace
+
+I imagined I worked in HR for a tech startup and wanted to create a webpage to use as part of a new employee orientation. The idea would be to have employees watch a fun video introducing them to the culture and best practices at the startup and then take a quiz on what they learned.
+
+I couldn't find any good example videos, so instead I used a clip from The Office and some made up questions.
+
+If I were to build this page (or realistically oversee someone else building it, which is more likely how my eventual job will work) I would of course have a log in so that employees would get credit for watching it, and probably also have additional content like workplace policies, etc.
+
+Demonstration Page:
+
+Youtube Video:
+
+
+
+Link to demonstration page (i.e. Github Pages)
+Link to YouTube reflection video
--- scripts/techplace.js
+++ scripts/techplace.js
@@ -1,12 +1,12 @@
 //Javascript
 
 //function to reveal techEtiquette101 Video and take the quiz button//
-document.querySelector("#videoBtn").addEventListener("click", function(){
-    document.querySelector("#techEtiquette101").style.display = "block"
-  });
-  // Important Elements
-  var quizBtnEl = document.querySelector('#officeQuiz');
-  var quizEl = document.querySelector('#quizQuestions');
+document.querySelector("#videoBtn").addEventListener("click", function() {
+  document.querySelector("#techEtiquette101").style.display = "block"
+});
+// Important Elements
+var quizBtnEl = document.querySelector('#officeQuiz');
+var quizEl = document.querySelector('#quizQuestions');
 
 //Array of test questions
 var quiz = [
@@ -48,42 +48,42 @@ right now this code works by printing out q1 and the answers for q1, then over
 writing it with q1 + q1 answers + q2 + q2answers, etc. That is inefficient and not
 really what I want to happen. If I have time, I want to see if I can fix it.*/
 
-quizBtnEl.addEventListener("click", function(){
+quizBtnEl.addEventListener("click", function() {
   var quizHTML = "";
   for (var i = 0; i < quiz.length; i++) {
     quizHTML += `<li id="">`;
     quizHTML += quiz[i].question;
     quizHTML += `</li>`;
-      var multipleHTML = "";
-      for (var j = 0; j < quiz[i].options.length; j++) {
-        multipleHTML += `<div class="radio">`;
-        multipleHTML += `<label>`;
-        multipleHTML += `<input type="radio" name="q-${i}" value="${quiz[i].options[j]}">`;
-        multipleHTML += quiz[i].options[j];
-        multipleHTML += `</label>`;
-        multipleHTML += `</div>`;
-      }
-      quizEl.innerHTML = quizHTML += multipleHTML;
+    var multipleHTML = "";
+    for (var j = 0; j < quiz[i].options.length; j++) {
+      multipleHTML += `<div class="radio">`;
+      multipleHTML += `<label>`;
+      multipleHTML += `<input type="radio" name="q-${i}" value="${quiz[i].options[j]}">`;
+      multipleHTML += quiz[i].options[j];
+      multipleHTML += `</label>`;
+      multipleHTML += `</div>`;
+    }
+    quizEl.innerHTML = quizHTML += multipleHTML;
   }
 });
 
 /*function to reveal submit button when "take the quiz" button is clicked.*/
-document.querySelector("#officeQuiz").addEventListener("click", function(){
-    document.querySelector("#checkQuiz").style.display = "block"
+document.querySelector("#officeQuiz").addEventListener("click", function() {
+  document.querySelector("#checkQuiz").style.display = "block"
 });
 
 /* function to check quiz answers and return messages.*/
-document.querySelector("#checkQuiz").addEventListener("click", function(){
+document.querySelector("#checkQuiz").addEventListener("click", function() {
   var total = 0;
   for (var i = 0; i < quiz.length; i++) {
     var answer1 = document.querySelector(`input[name=q-${i}]:checked`).value;
-      if (answer1 === quiz[i].answer) {
-        total++;
-      }
+    if (answer1 === quiz[i].answer) {
+      total++;
+    }
   }
   if (total == 3) {
     alert("Perfect score! Great job.");
+  } else {
+    alert(`You got ${total} out of 3. Maybe you should watch the video again.`);
   }
-  else {alert(`You got ${total} out of 3. Maybe you should watch the video again.`);
-}
 });
